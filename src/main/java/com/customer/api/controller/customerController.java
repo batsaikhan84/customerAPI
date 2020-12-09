e3a03a3467dac9ba6e7d9c60fd219275ca93cbb0
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-12-08 21:27:28 -0600

    put and delete mapping added

--- src/main/java/com/customer/api/controller/customerController.java
+++ src/main/java/com/customer/api/controller/customerController.java
@@ -1,17 +1,19 @@
 package com.customer.api.controller;
 
 import com.customer.api.Repository.CustomerRepository;
+import com.customer.api.exception.RecordNotFoundException;
 import com.customer.api.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class customerController {
     @Value("$spring.application.name")
@@ -24,16 +26,32 @@ public class customerController {
         return "home";
     }
     @GetMapping("/customers")
-    public List<Customer> getCustomers() {
-        return customerRepository.findAll();
+    public Page<Customer> getCustomers(Pageable pageable) {
+        return customerRepository.findAll(pageable);
     }
     @PostMapping("/customers")
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerRepository.save(customer);
     }
-
-//    @GetMapping("/customers/{id}")
-//    public ResponseEntity<Customer> getCustomer() {
-//        Customer customer = customerRepository.findById(id)
-//    }
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable(value = "id") long id) throws RecordNotFoundException {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new RecordNotFoundException("ID: " + id + " customer not found!"));
+        return ResponseEntity.ok().body(customer);
+    }
+    @DeleteMapping("/customers/{id}")
+    public Map<String, String> deleteCustomer(@PathVariable(value = "id") long id) throws RecordNotFoundException {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new RecordNotFoundException("Employee not found by ID: " + id));
+        customerRepository.delete(customer);
+        Map<String, String> deleteCustomer = new HashMap<>();
+        deleteCustomer.put("Person is deleted", "by id: " + id);
+        return deleteCustomer;
+    }
+    @PutMapping("/customers/{id")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id")long id, @RequestBody Customer customerDetails) throws RecordNotFoundException {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new RecordNotFoundException("ID " + id + " not found"));
+        customer.setName(customerDetails.getName());
+        customer.setEmail(customerDetails.getEmail());
+        final Customer updateCustomer = customerRepository.save(customer);
+        return ResponseEntity.ok(updateCustomer);
+    }
 }
